78d778bc54ae165da04d033d6f1fa5ed2b94e345
Márcio Castelo Branco <48799029+marcioclay@users.noreply.github.com>
2024-02-14 22:06:16 -0300

    Add files via upload

--- Exercícios/sequencial/desafio_3.py
+++ Exercícios/sequencial/desafio_3.py
@@ -1,9 +1,6 @@
-# Crie um script Python que leia dois números e tente mostrar a soma entre eles
+# Crie um programa que leia dois números e mostre a soma entre eles
 
-num1 = int(input("Primeiro número: \n"))
-num2 = int(input("Segundo número: \n"))
-print("A soma é:", num1 + num2)
-print("A soma é: %d!" % (num1 + num2))
-print("A soma é: {}!".format(num1 + num2))
-
-# Os três formatos de print aceitam operações entre variáveis
+n1 = int(input("Digite um número: \n"))
+n2 = input("Digite outro número: \n")
+s = n1 + n2
+print(s)
