8097d915ec846b4d3505149f9a61470d37db13c2
Márcio Castelo Branco <48799029+marcioclay@users.noreply.github.com>
2024-02-28 10:19:33 -0300

    Update parImpar.py

--- Exercícios/parImpar.py
+++ Exercícios/parImpar.py
@@ -1,3 +1,5 @@
+#Verificar se um número é par ou ímpar: Desenvolva um programa que receba um número como entrada e determine se ele é par ou ímpar.
+
 print('Par ou ímpar')
 
 num = int(input('Digite um número: '))
