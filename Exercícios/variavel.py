6ce35ba747a1a806d8673e3eafd8099a084c5b6d
Márcio Castelo Branco <48799029+marcioclay@users.noreply.github.com>
2024-02-29 09:47:10 -0300

    Create variavel.py

---
+++ Exercícios/variavel.py
@@ -0,0 +1,14 @@
+# Em Python, a necessidade de declarar explicitamente o tipo de dado de uma variável é dispensada por causa da tipagem dinâmica. 
+# Declaração de variáveis
+numero_inteiro = 10
+numero_float = 3.14
+texto = "Olá, mundo!"
+verdadeiro = True
+falso = False
+
+# Impressão dos valores das variáveis
+print(f"Número inteiro: {numero_inteiro}")
+print(f"Número float: {numero_float}")
+print(f"Texto: {texto}")
+print(f"Verdadeiro: {verdadeiro}")
+print(f"Falso: {falso}")
