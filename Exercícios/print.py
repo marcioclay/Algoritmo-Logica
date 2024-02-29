53b5a3e7e4a7c42e2c1a0b4df1d760e9ae8ad011
Márcio Castelo Branco <48799029+marcioclay@users.noreply.github.com>
2024-02-29 10:00:58 -0300

    Create print.py

---
+++ Exercícios/print.py
@@ -0,0 +1,11 @@
+# Imprimindo "Olá Mundo!" sem variável
+print("Olá Mundo!")
+
+# A função print() é utilizada para impressão na tela.
+# Ela pode receber vários argumentos, que serão concatenados e impressos em sequência.
+
+# Declarando uma variável do tipo string
+mensagem = "Olá Mundo!"
+
+# Imprimindo "Olá Mundo!" usando a variável
+print(mensagem)
