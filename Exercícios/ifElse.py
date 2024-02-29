504df1141baa83f37ed418a6d60ba206ef96f1cb
Márcio Castelo Branco <48799029+marcioclay@users.noreply.github.com>
2024-02-29 13:58:31 -0300

    Create ifElse.py

---
+++ Exercícios/ifElse.py
@@ -0,0 +1,15 @@
+""" 
+Crie um algoritmo que verifique se um determinado numero é maior que 10. """
+
+numero = int(input("Digite um número: "))
+
+if numero > 10:
+  print(f"O número {numero} é maior que 10.")
+else:
+  print(f"O número {numero} é menor ou igual a 10.")
+
+"""Explicação:
+
+A instrução if verifica se a condição numero > 10 é verdadeira.
+Se a condição for verdadeira, o bloco de código dentro do if é executado.
+Se a condição for falsa, o bloco de código dentro do else é executado."""
