0237aa65d97a7f2a7b43a6a6f0d3c92944e3704f
Márcio Castelo Branco <48799029+marcioclay@users.noreply.github.com>
2024-02-29 10:04:56 -0300

    Update print.py

--- Exercícios/print.py
+++ Exercícios/print.py
@@ -9,3 +9,11 @@ mensagem = "Olá Mundo!"
 
 # Imprimindo "Olá Mundo!" usando a variável
 print(mensagem)
+
+"""
+Observações:
+
+Este é um exemplo simples de como usar a função print() em Python.
+Você pode utilizar a função print() para imprimir qualquer tipo de dado, como números, listas, dicionários e objetos.
+É importante usar aspas simples ou duplas para delimitar a string que será impressa.
+"""
