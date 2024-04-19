bbff401c385a0fab40cc850ece7a4e87a95a45ad
Márcio Castelo Branco <48799029+marcioclay@users.noreply.github.com>
2024-04-19 08:15:55 -0300

    Update Cadastro.py

--- Exercícios/Cadastro.py
+++ Exercícios/Cadastro.py
@@ -1,32 +1,32 @@
-alunos=[]
-def menu():
-    print("1-Incluir","2-Excluir","3-Atualizar","4-Listar", "5-Sair")
-    opcao=int(input("Selecione opção: "))
-    while opcao!=5:
+alunos = []
 
-        if opcao==1:
-            nome=input("Digite nome do aluno: ")
+def menu():
+    print("1-Incluir", "2-Excluir", "3-Atualizar", "4-Listar", "5-Sair")
+    opcao = int(input("Selecione opção: "))
+    while opcao != 5:
+
+        if opcao == 1:
+            nome = input("Digite nome do aluno: ")
             alunos.append(nome)
             print(alunos)
-        elif opcao==2:
+        elif opcao == 2:
             nome = input("Digite nome do aluno que deseja excluir: ")
             alunos.remove(nome)
         elif opcao == 3:
             print(alunos)
-            nomealterar = input("Qual nome deseja remover? ")
-            posicao=0
-            for x in alunos:
-                if nomealterar==x:
-                    nome = input("Qual alteração deseja fazer? ")
-                    alunos[posicao] = nome
-                    break
-                else:
-                    print("Nome não encontrado")
-                posicao = posicao + 1
-        elif opcao==4:
+            nomeexcluir = input("Qual nome deseja remover? ")
+            posicao=alunos.index(nomeexcluir)
+            alunos.pop(posicao)
+            nome = input("Qual alteração deseja fazer? ")
+            alunos.insert(posicao,nome)
+
+        elif opcao == 4:
             print(alunos)
 
         listas = ["1-Incluir", "2-Excluir", "3-Atualizar", "4-Listar", "5-Sair"]
         opcao = int(input(f'{listas}\nSelecione opção: '))
+
+
+
 menu()
     
